53a2e6afe902dee10b95af9ff7b91b6e3547adb7
Ivan <mastertimer@mail.ru>
2023-02-12 05:11:09 +0300

    окно windows из basic

--- main.cpp
+++ main.cpp
@@ -268,32 +268,5 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 {
 	exe_path = get_exe_path(hInstance);
 	init_ui_elements();
-
-	static TCHAR szWindowClass[] = L"win64app";
-	WNDCLASSEX wcex;
-	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
-	wcex.lpfnWndProc = WndProc;
-	wcex.cbClsExtra = 0;
-	wcex.cbWndExtra = 0;
-	wcex.hInstance = hInstance;
-	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(101)); // 101 из resourse.h
-	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground = 0;
-	wcex.lpszMenuName = NULL;
-	wcex.lpszClassName = szWindowClass;
-	wcex.hIconSm = 0;
-	if (!RegisterClassEx(&wcex)) return 2;
-	HWND hWnd = CreateWindow(szWindowClass, L"mutator", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 1600, 800,
-		NULL, NULL, hInstance, NULL);
-	if (!hWnd) return 3;
-	ShowWindow(hWnd, nCmdShow);
-	UpdateWindow(hWnd);
-	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-	return (int)msg.wParam;
+	return run_windows_app(L"se", hInstance, WndProc, nCmdShow);
 }
